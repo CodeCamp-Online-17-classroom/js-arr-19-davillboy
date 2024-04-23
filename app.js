4b5cd8920907309b0d35e7df309f7c81ef116910
davillboy <71083807+davillboy@users.noreply.github.com>
2024-04-23 20:36:03 +0700

    first change

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 // write code here
+
+function checkEmptyObj(obj) {
+
+  if (typeof obj !== "object" || obj === null) {
+    return false; 
+  }
+
+  for (let key in obj) {
+    return false;
+  }
+
+  return true;
+}
+
+console.log(checkEmptyObj({})); // true
+console.log(checkEmptyObj({ name: "John", age: 30 })); // false
+console.log(checkEmptyObj(null)); // false
+console.log(checkEmptyObj([])); // false
+console.log(checkEmptyObj(123)); // false
